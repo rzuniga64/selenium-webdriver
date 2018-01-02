9f9dc494fe6b10ad0149bf783009147ffca45e01
Raul Zuniga <rzuniga64@gmail.com>
2018-01-02 12:06:04 -0600

    Add a findby example test.

---
+++ src/test/java/webdriver/interrogation/findby/AFirstFindByExampleTest.java
@@ -0,0 +1,35 @@
+package webdriver.interrogation.findby;
+
+import webdriver.drivermanager.Driver;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import webdriver.drivermanager.Driver;
+
+import static org.junit.Assert.assertEquals;
+
+public class AFirstFindByExampleTest {
+
+    static WebDriver driver;
+
+    @BeforeClass
+    public static void createDriverAndVisitTestPage(){
+        //driver = new FirefoxDriver();
+        driver = Driver.get("selenium2basics.webdriver","HTMLUNIT" );
+        driver.get("http://www.compendiumdev.co.uk/" + "selenium/find_by_playground.php");
+    }
+
+    @Test
+    public void findByID(){
+        WebElement cParagraph = driver.findElement(By.id("p3"));
+        assertEquals("This is c paragraph text", cParagraph.getText());
+    }
+
+    @AfterClass
+    public static void closeBrowser(){
+        //driver.quit();
+    }
+}
