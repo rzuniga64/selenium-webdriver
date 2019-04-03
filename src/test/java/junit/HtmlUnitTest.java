e3363b3601c1a88e84e061272e8221cd98e6c3c1
Raul Zuniga <rzuniga64@gmail.com>
2019-04-02 21:25:41 -0500

    Update.

--- src/test/java/junit/HtmlUnitTest.java
+++ src/test/java/junit/HtmlUnitTest.java
@@ -2,6 +2,7 @@ package junit;
 
 import org.junit.Assert;
 import org.junit.Test;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.htmlunit.HtmlUnitDriver;
 
 public class HtmlUnitTest {
@@ -9,7 +10,7 @@ public class HtmlUnitTest {
     @Test
     public void startWebDriver(){
 
-        final HtmlUnitDriver driver = new HtmlUnitDriver();
+        final WebDriver driver = new HtmlUnitDriver();
         driver.navigate().to("http://seleniumsimplified.com");
 
         Assert.assertTrue("title should start differently",
