98993f6da11a93ca2b69133b07336df8e6e4bd14
Raul Zuniga <rzuniga64@gmail.com>
2019-04-02 21:33:20 -0500

    Update.

--- src/test/java/junit/FirefoxTest.java
+++ src/test/java/junit/FirefoxTest.java
@@ -21,6 +21,9 @@ public class FirefoxTest {
     @Test
     public void startWebDriver(){
 
+        // WebDriver is used here because we may want to inject the driver. So have the driver itself instantiated
+        // somewhere else because we may want our tests to run across Firefox, Chrome, Opera, HtmlUnit, or a remote
+        // Web Driver. The driver may have additional commands on it, but that's for each driver to decide.
         WebDriver driver = new FirefoxDriver();
 
         driver.navigate().to("http://seleniumsimplified.com");
