6a51847c92c7e3f57e56d40758f7c5eae3ac830f
Raul Zuniga <rzuniga64@gmail.com>
2019-04-03 18:03:34 -0500

    Add Junit example.

--- src/test/java/junit/MyClassTest.java
+++ src/test/java/junit/MyClassTest.java
@@ -4,6 +4,12 @@ import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 
+/**
+ *  MyClassTest class.
+ *  - in order to a test to be considered a test it must be under the src/test folder.
+ *  - In order for a method to be treated as a test, in the Junit 4 version, we annotate the test with @Test.
+ *  - You must have 'Test' in the method name.
+ */
 public class MyClassTest {
 
     @Test
