8810ca599795512fde283bb70a9e367f40dc371a
Raul Zuniga <rzuniga64@gmail.com>
2019-04-03 22:51:02 -0500

    Update.

--- src/test/java/webdriver/basics/driver/HtmlUnitTest.java
+++ src/test/java/webdriver/basics/driver/HtmlUnitTest.java
@@ -5,6 +5,27 @@ import org.junit.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.htmlunit.HtmlUnitDriver;
 
+/**
+ * We need to create a driver before we can do anything.
+ * HtmlUnitDriver is a headless browser implemented using HtmlUnit
+ * http://htmlunit.sourceforge.net/
+ *
+ * Advantages:
+ * - Fast
+ * - good for simple testing
+ *
+ * Disadvantages:
+ * - does not handle JavaScript as well as a full browser - but it is getting better
+ * - you need to add HtmlUnitDriver to your pom.xml
+ *   https://github.com/SeleniumHQ/htmlunit-driver
+ *
+ <dependency>
+ <groupId>org.seleniumhq.selenium</groupId>
+ <artifactId>htmlunit-driver</artifactId>
+ <version>${htmlunitdriver.version}</version>
+ </dependency>
+ *
+ */
 public class HtmlUnitTest {
 
     @Test
@@ -19,6 +40,6 @@ public class HtmlUnitTest {
         Assert.assertTrue("title should start differently",
                 driver.getTitle().startsWith("Selenium Simplified"));
 
-        driver.quit();
+        // don't need to close an HtmlUnitDriver, garbage collection will take care of it.
     }
 }
