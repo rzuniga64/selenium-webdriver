55eb728f293cf349faeab2a8b8e88120ae8ddd13
Raul Zuniga <rzuniga64@gmail.com>
2019-04-03 22:04:40 -0500

    Update to use a factory to generate the driver.

--- src/test/java/webdriver/basics/navigation/NavigationExampleTest.java
+++ src/test/java/webdriver/basics/navigation/NavigationExampleTest.java
@@ -8,7 +8,12 @@ import org.openqa.selenium.WebDriver;
 
 import static org.junit.Assert.assertTrue;
 
-
+/**
+ *  NavigationExampleTest class.
+ *  We are using a factory class to get an instance of a WebDriver.
+ *  The driver.quit() is removed because the static class uses some Java functionality to create a JVM shutdown hook.
+ *  So when the JVM stops, the driver stops automatically. 
+ */
 public class NavigationExampleTest {
 
     private static WebDriver driver;
@@ -21,6 +26,7 @@ public class NavigationExampleTest {
 
     @Test
     public void navigateWithNavigateTo(){
+
         driver.navigate().to("http://www.compendiumdev.co.uk/selenium/search.php");
         assertTrue(driver.getTitle().startsWith("Selenium Simplified Search Engine"));
     }
