b7fa58c56e5c31346ecbdc9df1b9c53074c2e9d1
Raul Zuniga <rzuniga64@gmail.com>
2019-04-05 14:38:32 -0500

    Update.

--- src/test/java/webdriver/basics/interrogation/WebElementInterrogationTest.java
+++ src/test/java/webdriver/basics/interrogation/WebElementInterrogationTest.java
@@ -7,6 +7,8 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import webdriver.drivermanager.Driver;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertEquals;
 
 /**
@@ -33,7 +35,8 @@ public class WebElementInterrogationTest {
         // WebElement represents an object in the DOM.
         WebElement para1 = driver.findElement(By.id("para1"));
 
-        // Inspect the WebElement using teh exposed methods.
+        // Inspect the WebElement using the exposed methods.
         assertEquals(para1.getText(),"A paragraph of text");
+        assertThat(para1.getText(), is("A paragraph of text"));
     }
 }
