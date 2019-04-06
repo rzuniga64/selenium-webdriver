ea941f9510120813903b4f92cc69f634c1a05652
Raul Zuniga <rzuniga64@gmail.com>
2019-04-06 15:27:13 -0500

    Update.

--- src/test/java/webdriver/basics/interrogation/examples/FindByCSSSelectorPathsExampleTest.java
+++ src/test/java/webdriver/basics/interrogation/examples/FindByCSSSelectorPathsExampleTest.java
@@ -9,6 +9,18 @@ import org.openqa.selenium.WebDriver;
 
 import static org.junit.Assert.assertEquals;
 
+/**
+ *  FindByCSSSelectorPathsExampleTest class.
+ *  A > B           B directly under A, e.g. <A><B/></A>
+ *  A B             Descendent
+ *                  - Selectors separated by space, i.e. "this then that"
+ *                  - Any degree of separation.
+ *                  - e.g. "div li" would match but "div > li" would not
+ *  A + B           B siblings of A
+ *  B: first child  Every B which is the first child of something.
+ *
+ *  For more selectors see the reference
+ */
 public class FindByCSSSelectorPathsExampleTest {
 
     private static WebDriver driver;
@@ -20,7 +32,11 @@ public class FindByCSSSelectorPathsExampleTest {
         driver = Driver.get("http://www.compendiumdev.co.uk/selenium/find_by_playground.php");
     }
 
-
+    /**
+     *  directDescendant method.
+     *  Look for any paragraph that is directly under a div.
+     *  It should not return any li directly under a div.
+     */
     @Test
     public void directDescendant(){
 
@@ -29,6 +45,11 @@ public class FindByCSSSelectorPathsExampleTest {
 
     }
 
+    /**
+     *  anyDescendant method.
+     *  Look for any paragraph under any div in the hierarchy.
+     *  Look for any li under any div in the hierarchy.
+     */
     @Test
     public void anyDescendant(){
 
@@ -36,19 +57,25 @@ public class FindByCSSSelectorPathsExampleTest {
         assertEquals("div li", 25, driver.findElements(By.cssSelector("div li")).size());
     }
 
+    /**
+     *  siblingOfPreceding method.
+     *  Looks for any li.
+     *  Looks for any li that has a li as a sibling. li is in a big list so li + li will skip the first li.
+     */
     @Test
     public void siblingOfPreceding(){
 
-        assertEquals("li + li", 24, driver.findElements(By.cssSelector("li + li")).size());
-
-        // li are in a big list so li + li skipped the first one
         assertEquals("li", 25, driver.findElements(By.cssSelector("li")).size());
+        assertEquals("li + li", 24, driver.findElements(By.cssSelector("li + li")).size());
     }
 
+    /**
+     *  firstChild method.
+     *  Get the first child or the first li that is a child of an WebElement.
+     */
     @Test
     public void firstChild(){
 
-        // get the first child li we missed out in the test before
         assertEquals("li:first-child", 1,
                 driver.findElements(By.cssSelector("li:first-child")).size());
     }
