f601b68e6b72650a146122443aeee69326dba407
Raul Zuniga <rzuniga64@gmail.com>
2019-04-05 20:16:31 -0500

    Update.

--- src/test/java/webdriver/basics/interrogation/findby/FindByChainingExampleTest.java
+++ src/test/java/webdriver/basics/interrogation/findby/FindByChainingExampleTest.java
@@ -1,6 +1,5 @@
 package webdriver.basics.interrogation.findby;
 
-import webdriver.drivermanager.Driver;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
@@ -8,16 +7,17 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.pagefactory.ByChained;
+import webdriver.drivermanager.Driver;
 
-import static org.junit.Assert.assertEquals;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
 
 /**
  *  Chaining .findElements(s)
- *  findElement(by.id(".")).findElement(By.name("."))
- *  e.g.
- *  WebElement element = driver.findElement(By.id("div1")).
- *                              findElement(By.name("pName3")).
- *                              findElement(By.tagName("a"));
+ *  findElement(by.id(".")).findElement(By.name(".")), e.g.
+ *  WebElement element = driver.findElement(By.id("div1"))
+ *                              .findElement(By.name("pName3"))
+ *                              .findElement(By.tagName("a"));
  *  - Can use any By locator strategy
  *  - Cannot Chain .findElements() as it return a List of Web Elements.
  *
@@ -25,7 +25,7 @@ import static org.junit.Assert.assertEquals;
  *  - ByChained is a support class
  *    import org.openqa.selenium.support.pagefactory.ByChained;
  *  - ByChained extends By (it is a By)
- *  - Instantiate it and pass in teh By objects
+ *  - Instantiate it and pass in the By objects
  *
  *  Have to instantiate not use statically, takes list of By objects
  *  element = driver.findElement(
@@ -51,10 +51,14 @@ public class FindByChainingExampleTest {
 
     @BeforeClass
     public static void createDriverAndVisitTestPage(){
-        driver = Driver.get("selenium2basics.webdriver","HTMLUNIT" );
+
+        driver = Driver.get("webdriver.chrome.driver","CHROME" );
         driver = Driver.get("http://www.compendiumdev.co.uk/selenium/find_by_playground.php");
     }
 
+    /**
+     *  Chaining using findElement By method.
+     */
     @Test
     public void chainingWithFindElement(){
 
@@ -62,11 +66,12 @@ public class FindByChainingExampleTest {
                                     findElement(By.name("pName3")).
                                     findElement(By.tagName("a"));
 
-        assertEquals("expected a different id",
-                     "a3",
-                     element.getAttribute("id"));
+        assertThat("expected a different id", "a3", is(element.getAttribute("id")));
     }
 
+    /**
+     *  Chaining by using byChained By method.
+     */
     @Test
     public void chainingWithSupportClassByChained(){
 
@@ -77,9 +82,7 @@ public class FindByChainingExampleTest {
                                 By.name("pName9"),
                                 By.tagName("a")));
 
-        assertEquals("expected a different id",
-                     "a9",
-                     element.getAttribute("id"));
+        assertThat("expected a different id", "a9", is(element.getAttribute("id")));
     }
 
     @AfterClass
