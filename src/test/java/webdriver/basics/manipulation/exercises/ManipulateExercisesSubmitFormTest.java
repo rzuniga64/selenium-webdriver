bdd7694cc30393e1c00e858a9fbfe14578cfe24e
Raul Zuniga <rzuniga64@gmail.com>
2019-04-07 13:59:14 -0500

    Update.

--- src/test/java/webdriver/basics/manipulation/exercises/ManipulateExercisesSubmitFormTest.java
+++ src/test/java/webdriver/basics/manipulation/exercises/ManipulateExercisesSubmitFormTest.java
@@ -10,13 +10,12 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 /**
  *  using http://compendiumdev.co.uk/selenium/basic_html_form.html
  *
- *  1. Submit form and assert page title changes
- *  2. Clear, then type comments, submit form and check output
- *  3. Submit form with radio 2 selected
- *  4. Submit form with only checkbox 1 selected
- *  5. Submit form with drop down item 5 selected
- *  6. Submit form with multiple select 1, 2, & 3
- *  7  Submit with a file, and check name on output
+ *  1. Submits form with button click and assert page title changes.
+ *  2. Submits form with submit and assert page title changes.
+ *  3. Submits form with form submit and assert page title changes.
+ *  4. Submits form with input of type 'password' and assert page title changes.
+ *     Test that you can submit on any form element.
+ *  5. Submits form with keyboard key ENTER and assert page title changes.
  */
 public class ManipulateExercisesSubmitFormTest {
 
@@ -24,56 +23,63 @@ public class ManipulateExercisesSubmitFormTest {
 
     @Before
     public void setup(){
-        driver = Driver.get("selenium2basics.webdriver", "CHROME");
+
+        driver = Driver.get("webdriver.chrome.driver", "CHROME");
         driver = Driver.get("http://compendiumdev.co.uk/selenium/basic_html_form.html");
     }
 
+    /**
+     *  Submits form with button click and assert page title changes.
+     */
     @Test
     public void submitFormWithButtonClick(){
 
-        WebElement submitButton;
-        submitButton = driver.findElement(By.cssSelector("input[type='submit'][name='submitbutton']"));
-
+        WebElement submitButton = driver.findElement(By.cssSelector("input[type='submit'][name='submitbutton']"));
         submitButton.click();
         new WebDriverWait(driver,10).until(ExpectedConditions.titleIs("Processed Form Details"));
     }
 
+    /**
+     *  Submits form with submit and assert page title changes.
+     */
     @Test
          public void submitFormWithButtonSubmit(){
 
-        WebElement submitButton;
-        submitButton = driver.findElement(By.cssSelector("input[type='submit'][name='submitbutton']"));
-
+        WebElement submitButton = driver.findElement(By.cssSelector("input[type='submit'][name='submitbutton']"));
         submitButton.submit();
         new WebDriverWait(driver,10).until(ExpectedConditions.titleIs("Processed Form Details"));
     }
 
+    /**
+     *  Submits form with form submit and assert page title changes.
+     */
     @Test
     public void submitFormWithFormSubmit(){
 
-        WebElement submitForm;
-        submitForm = driver.findElement(By.id("HTMLFormElements"));
-
+        WebElement submitForm = driver.findElement(By.id("HTMLFormElements"));
         submitForm.submit();
         new WebDriverWait(driver,10).until(ExpectedConditions.titleIs("Processed Form Details"));
     }
 
+    /**
+     *  Submits form with input of type 'password' and assert page title changes.
+     *  Test that you can submit on any form element.
+     */
     @Test
     public void iCanActuallySubmitOnAnyFormElement(){
 
-        WebElement passwordInput;
-        passwordInput = driver.findElement(By.cssSelector("input[type='password']"));
-
+        WebElement passwordInput = driver.findElement(By.cssSelector("input[type='password']"));
         passwordInput.submit();
         new WebDriverWait(driver,10).until(ExpectedConditions.titleIs("Processed Form Details"));
     }
 
+    /**
+     *  Submits form with keyboard key ENTER and assert page title changes.
+     */
     @Test
     public void submitButtonWithKeyPress(){
 
-        WebElement submitButton;
-        submitButton = driver.findElement(By.cssSelector("input[type='submit'][name='submitbutton']"));
-
+        WebElement submitButton = driver.findElement(By.cssSelector("input[type='submit'][name='submitbutton']"));
         submitButton.sendKeys(Keys.ENTER);
         // if enter does not work then try SPACE
         //submitButton.sendKeys(Keys.SPACE);
