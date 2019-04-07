2eb6982387f9e2f4fdfed44762857b8a4871a973
Raul Zuniga <rzuniga64@gmail.com>
2019-04-07 14:31:02 -0500

    Update.

--- src/test/java/webdriver/basics/manipulation/exercises/ManipulateExercisesCommentsTest.java
+++ src/test/java/webdriver/basics/manipulation/exercises/ManipulateExercisesCommentsTest.java
@@ -1,47 +1,59 @@
 package webdriver.basics.manipulation.exercises;
 
-import webdriver.drivermanager.Driver;
-import org.junit.Before;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import webdriver.drivermanager.Driver;
 
 import static org.junit.Assert.assertEquals;
 
+/**
+ *  ManipulateExercisesCommentsTest class.
+ *  Clear, then type comments, submit form and check output.
+ */
 public class ManipulateExercisesCommentsTest {
 
     private static WebDriver driver;
 
-    @Before
-    public void setup(){
-        driver = Driver.get("selenium2basics.webdriver", "CHROME");
+    @BeforeClass
+    public static void setup(){
+
+        driver = Driver.get("webdriver.chrome.driver", "CHROME");
         driver = Driver.get("http://compendiumdev.co.uk/selenium/basic_html_form.html");
     }
 
+    /**
+     *  submitFormDefaultComments method.
+     *  Submit the default comments in the comments section of a form.
+     */
     @Test
     public void submitFormDefaultComments(){
 
-        WebElement submitButton;
-
-        submitButton = driver.findElement(By.cssSelector("input[type='submit'][name='submitbutton']"));
+        WebElement submitButton = driver.findElement(By.cssSelector("input[type='submit'][name='submitbutton']"));
         submitButton.click();
         new WebDriverWait(driver,10).until(ExpectedConditions.titleIs("Processed Form Details"));
 
-        WebElement commentsResults;
-        commentsResults = driver.findElement(By.id("_valuecomments"));
+        WebElement commentsResults = driver.findElement(By.id("_valuecomments"));
         assertEquals("Expected default comments", "Comments...", commentsResults.getText());
     }
 
+    /**
+     *  submitFormWithMyComments method.
+     *  Clear, then type comments, submit form and check output.
+     */
     @Test
     public void submitFormWithMyComments(){
 
         WebElement submitButton;
         WebElement commentTextArea;
-
+        WebElement commentsResults;
         String myCommentString = "This is my comment";
+
         commentTextArea = driver.findElement(By.name("comments"));
         commentTextArea.clear();
         commentTextArea.sendKeys(myCommentString);
@@ -50,8 +62,12 @@ public class ManipulateExercisesCommentsTest {
         submitButton.click();
         new WebDriverWait(driver,10).until(ExpectedConditions.titleIs("Processed Form Details"));
 
-        WebElement commentsResults;
         commentsResults = driver.findElement(By.id("_valuecomments"));
         assertEquals("Expected default comments", myCommentString, commentsResults.getText());
+    }
+
+    @AfterClass
+    public static void closeBrowser(){
+        //driver.quit();
     }
 }
