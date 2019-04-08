290276e32bd08d9b76497fe29b270c4789a74a5c
Raul Zuniga <rzuniga64@gmail.com>
2019-04-08 16:55:04 -0500

    Update.

--- src/test/java/webdriver/basics/manipulation/alerts/AlertHandlingExampleTest.java
+++ src/test/java/webdriver/basics/manipulation/alerts/AlertHandlingExampleTest.java
@@ -1,23 +1,35 @@
 package webdriver.basics.manipulation.alerts;
 
-import webdriver.drivermanager.Driver;
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import webdriver.drivermanager.Driver;
 
 import static org.junit.Assert.assertEquals;
 
+/**
+ *  AlertHandlingExampleTest class.
+ *  Handle Aerts with
+ *  driver.switchTo().alert
+ *  .getText()
+ *  .dismiss()
+ *  .accept()
+ *  .sendKeys(String)
+ *
+ *  .alert() returns an Alert object
+ */
 public class AlertHandlingExampleTest {
 
     private static WebDriver driver;
 
-
     @Before
     public void setup(){
-        driver = Driver.get("selenium2basics.webdriver", "CHROME");
-        driver = Driver.get("http://compendiumdev.co.uk/selenium/alert.html");
+
+        driver = Driver.get("webdriver.chrome.driver", "CHROME");
+        driver.navigate().to("http://compendiumdev.co.uk/selenium/alert.html");
     }
 
     @Test
@@ -29,6 +41,11 @@ public class AlertHandlingExampleTest {
         String alertMessage = "I am an alert box!";
         assertEquals(alertMessage,driver.switchTo().alert().getText());
         driver.switchTo().alert().accept();
+    }
+
+    @After
+    public void closeBrowser(){
+        //driver.quit();
     }
 }
 
