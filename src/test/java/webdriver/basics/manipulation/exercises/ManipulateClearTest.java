c2148f5a865fc7313b60f911c439146bf402237e
Raul Zuniga <rzuniga64@gmail.com>
2019-04-08 11:11:49 -0500

    Update.

--- src/test/java/webdriver/basics/manipulation/exercises/ManipulateClearTest.java
+++ src/test/java/webdriver/basics/manipulation/exercises/ManipulateClearTest.java
@@ -1,5 +1,6 @@
 package webdriver.basics.manipulation.exercises;
 
+import org.junit.After;
 import webdriver.drivermanager.Driver;
 import org.junit.Before;
 import org.junit.Test;
@@ -11,13 +12,17 @@ import org.openqa.selenium.support.ui.Select;
 
 import static org.junit.Assert.fail;
 
-public class ManipulateWhatHappensIfTest {
+/**
+ *  ManipulateClearTest class. Try to clear a checkbox, a multiselect, a dropdown select, and a radio button.
+ */
+public class ManipulateClearTest {
 
     private static WebDriver driver;
 
     @Before
     public void setup(){
-        driver = Driver.get("selenium2basics.webdriver", "CHROME");
+
+        driver = Driver.get("webdriver.chrome.driver", "CHROME");
         driver = Driver.get("http://compendiumdev.co.uk/selenium/basic_html_form.html");
     }
 
@@ -26,20 +31,14 @@ public class ManipulateWhatHappensIfTest {
     public void canIClearACheckbox(){
 
         WebElement checkBox1 = driver.findElement(By.cssSelector("input[value='cb1']"));
-
-        // select it
-        if(!checkBox1.isSelected()){
-            checkBox1.click();
-        }
+        if(!checkBox1.isSelected()) checkBox1.click();
 
         try{
             checkBox1.clear();
 
             // HTMLUnit does not throw an exception when trying to clear this element, but other drivers do
             if (Driver.currentBrowser() == Driver.BrowserName.HTMLUNIT){
-                if (!checkBox1.isSelected()){
-                   fail("can now clear a checkbox");
-                }
+                if (!checkBox1.isSelected()) fail("can now clear a checkbox");
             } else {
                 fail("Expected an exception as you can't clear a checkbox");
             }
@@ -51,9 +50,7 @@ public class ManipulateWhatHappensIfTest {
     @Test
     public void canIClearAMultiSelect(){
 
-        WebElement multiSelect;
-
-        multiSelect = driver.findElement(By.cssSelector("select[multiple='multiple']"));
+        WebElement multiSelect = driver.findElement(By.cssSelector("select[multiple='multiple']"));
 
         try{
             multiSelect.clear();
@@ -62,11 +59,9 @@ public class ManipulateWhatHappensIfTest {
             if(Driver.currentBrowser() == Driver.BrowserName.HTMLUNIT){
 
                 Select multi = new Select(multiSelect);
-
-                if(multi.getAllSelectedOptions().size() == 0){
-                    fail("can now clear a multi select");
-                }
+                if(multi.getAllSelectedOptions().size() == 0)fail("can now clear a multi select");
             } else{
+
                 fail("Expected an exception as you can't clear a multi select");
             }
         } catch(WebDriverException e){
@@ -77,9 +72,7 @@ public class ManipulateWhatHappensIfTest {
     @Test
     public void canIClearADropDownSelect(){
 
-        WebElement dropDownSelect;
-
-        dropDownSelect = driver.findElement(By.cssSelector("select[name='dropdown']"));
+        WebElement dropDownSelect = driver.findElement(By.cssSelector("select[name='dropdown']"));
 
         try{
             dropDownSelect.clear();
@@ -88,39 +81,37 @@ public class ManipulateWhatHappensIfTest {
             if(Driver.currentBrowser() == Driver.BrowserName.HTMLUNIT){
 
                 Select dropDown = new Select(dropDownSelect);
-
-                if(dropDown.getAllSelectedOptions().size() == 0){
-                    fail("can now clear a drop down select");
-                }
+                if(dropDown.getAllSelectedOptions().size() == 0)fail("can now clear a drop down select");
             } else{
+
                 fail("Expected an exception as you can't clear a drop down");
             }
         } catch(WebDriverException e){
-            // expected an exception, you can't clear a drop down
-            // you have to click to remove items
+            // expected an exception, you can't clear a drop down. You have to click to remove items.
         }
     }
 
     @Test
     public void canIClearARadiobutton(){
 
-        WebElement radioButton;
-
-        radioButton = driver.findElement(By.cssSelector("input[value='rd2']"));
+        WebElement radioButton = driver.findElement(By.cssSelector("input[value='rd2']"));
 
         try{
             radioButton.clear();
 
             // neither HTMLUnit throw an exception when trying to clear this element, but other drivers do
             if(Driver.currentBrowser() == Driver.BrowserName.HTMLUNIT){
-                if(!radioButton.isSelected()){
-                    fail("can now clear a radio button");
-                }
+                if(!radioButton.isSelected())fail("can now clear a radio button");
             } else{
                 fail("Expected an exception as you can't clear a radio button");
             }
         } catch(WebDriverException e){
             // expected an exception, you can't clear a radio button you have to click on another to remove selected
         }
+    }
+
+    @After
+    public void closeBrowser(){
+        driver.close();
     }
 }
