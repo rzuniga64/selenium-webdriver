795cfb9444dcfad80a8b5546bbaee42c599047ea
Raul Zuniga <rzuniga64@gmail.com>
2019-04-08 19:39:54 -0500

    Update.

--- src/test/java/webdriver/basics/manipulation/frames/FrameFaqTest.java
+++ src/test/java/webdriver/basics/manipulation/frames/FrameFaqTest.java
@@ -1,39 +1,51 @@
 package webdriver.basics.manipulation.frames;
 
-import webdriver.drivermanager.Driver;
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
+import webdriver.drivermanager.Driver;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.fail;
 
+/**
+ *  FrameFaqTest class.
+ */
 public class FrameFaqTest {
 
     private static WebDriver driver;
 
     @Before
     public void setup(){
-        driver = Driver.get("selenium2basics.webdriver", "CHROME");
-        driver = Driver.get("http://www.compendiumdev.co.uk/selenium/frames");
+
+        driver = Driver.get("webdriver.chrome.driver", "CHROME");
+        driver.navigate().to("http://www.compendiumdev.co.uk/selenium/frames");
     }
 
+    /**
+     *  Use switchTo method to switch to a frame before finding an element.
+     */
     @Test
     public void whatHappensIfIDoNotSwitchTo(){
 
         assertEquals("Frameset Example Title (Example 6)",driver.getTitle());
 
         // if you remove the switchTo then it won't find the element below
-        //driver.switchTo().frame("menu");
+        driver.switchTo().frame("menu");
         try{
             driver.findElement(By.cssSelector("a[href='frames_example_1.html']")).click();
-            fail("I did not expect to find this");
+            //fail("I did not expect to find this");
 
-        }catch(NoSuchElementException e){
+        } catch(NoSuchElementException e){
             // ignore the exception we expected
             e.printStackTrace();
         }
+    }
+
+    @After
+    public void closebrowser() {
+        driver.close();
     }
 }
