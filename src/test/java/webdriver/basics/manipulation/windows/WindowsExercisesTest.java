ce07b011ffff62db3c6b829570f079d8455a4e17
Raul Zuniga <rzuniga64@gmail.com>
2019-04-08 21:58:39 -0500

    Update.

--- src/test/java/webdriver/basics/manipulation/windows/WindowsExercisesTest.java
+++ src/test/java/webdriver/basics/manipulation/windows/WindowsExercisesTest.java
@@ -1,46 +1,63 @@
 package webdriver.basics.manipulation.windows;
 
-import webdriver.drivermanager.Driver;
+import org.junit.AfterClass;
+import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import webdriver.drivermanager.Driver;
 
 import java.util.Iterator;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
-
+import static org.openqa.selenium.support.ui.ExpectedConditions.elementToBeClickable;
+import static org.openqa.selenium.support.ui.ExpectedConditions.titleContains;
 /**
+ *
+ *  WindowsExercisesTest class.
  *  Using http://compendiumdev.co.uk/selenium/frames/index.html
  *  - Amend the example, WindowsExampleTest, to switch back to the frame window and demonstrate that you switched control.
  *  - Two of the links on the content frame can be switched to by name (eviltester.com and Componendium Developement)
  *    0pen both them and switch between all three windows (two of them using switchTo("name))
  *  - Close each window when done with it.
+ *
+ *  Useful link on window/frame names http://www.webreference.com/js/tutorial1/names.html.
  */
 public class WindowsExercisesTest {
 
     private static WebDriver driver;
-    // useful link on window/frame names http://www.webreference.com/js/tutorial1/names.html
+    private WebDriverWait wait;
 
-    @Test
-    public void switchBetweenWindows(){
+    @Before
+    public void setup() {
 
         driver = Driver.get("selenium2basics.webdriver", "CHROME");
         //Driver.quit(); // dodgy in a suite - close everything down and start again
-        WebDriver driver = Driver.get(
-                    "http://www.compendiumdev.co.uk/selenium/frames");
+        driver.navigate().to("http://www.compendiumdev.co.uk/selenium/frames");
+        wait = new WebDriverWait(driver, Driver.DEFAULT_TIMEOUT_SECONDS);
+    }
+
+    /**
+     *  switchBetweenWindows method.
+     *  Switch back to the frame window and demonstrate that you switched control.
+     *  Two of the links on the content frame can be switched to by name (eviltester.com and Componendium Developement)
+     *  Open both them and switch between all three windows (two of them using switchTo("name))
+     *  Close each window when done with it
+     */
+    @Test
+    public void switchBetweenWindows(){
 
         String framesWindowHandle = driver.getWindowHandle();
 
         driver.switchTo().frame("content");
         driver.findElement(By.cssSelector("a[href='http://www.seleniumsimplified.com']")).click();
 
-        String newWindowHandle=framesWindowHandle;
+        String newWindowHandle = framesWindowHandle;
 
+        // Find the handle for the new window
         Iterator<String> aHandle = driver.getWindowHandles().iterator();
-        // forgot to add hasNext, could lead to an infinite loop
         while(newWindowHandle.equals(framesWindowHandle) && aHandle.hasNext()){
             newWindowHandle = aHandle.next();
         }
@@ -50,45 +67,39 @@ public class WindowsExercisesTest {
         // for Marionette Geckodriver need to switchTo defaultContent to check title
         driver.switchTo().defaultContent();
         // added to see if this fixes synch problem when run in suite
-        new WebDriverWait(driver,5L).until(ExpectedConditions.titleContains("Selenium Simplified"));
-
-        assertTrue("Expected Selenium Simplified site",
-                driver.getTitle().contains("Selenium Simplified"));
+        wait.until(titleContains("Selenium Simplified"));
+        assertTrue("Expected Selenium Simplified site", driver.getTitle().contains("Selenium Simplified"));
 
         driver.switchTo().window(framesWindowHandle);
 
         // for Marionette Geckodriver need to switchTo defaultContent to check title
         //driver.switchTo().defaultContent();
 
-        assertTrue("Expected Frames site",
-                driver.getTitle().contains("Frameset Example"));
-
+        assertTrue("Expected Frames site", driver.getTitle().contains("Frameset Example"));
         driver.switchTo().window(newWindowHandle);
         driver.close();
         driver.switchTo().window(framesWindowHandle);
-
     }
 
-    // This fails on IE
+    /**
+     *  switchToByName method. This fails on IE>
+     *  Switch back to the frame window and demonstrate that you switched control.
+     *  Two of the links on the content frame can be switched to by name (eviltester.com and Componendium Developement)
+     *  Open both them and switch between all three windows (two of them using switchTo("name))
+     *  Close each window when done with it
+     */
     @Test
     public void switchToByName(){
-
-        driver = Driver.get("selenium2basics.webdriver", "CHROME");
-        //Driver.quit(); // dodgy in a suite - close everything down and start again
-        WebDriver driver = Driver.get(
-                "http://www.compendiumdev.co.uk/selenium/frames");
 
         String framesWindowHandle = driver.getWindowHandle();
 
         driver.switchTo().frame("content");
         driver.findElement(By.cssSelector("a[id='goevil']")).click();
 
-        // Firefox started failing here - need to sync on the linnk being available
-        new WebDriverWait(driver,10).until(
-                ExpectedConditions.elementToBeClickable(
-                        By.cssSelector("a[target='compdev']")));
+        // Firefox started failing here - need to sync on the link being available
+        wait.until(elementToBeClickable(By.cssSelector("a[target='compdev']")));
 
-        // IE Fails at this point, suspect I need to change context back to allow a reclick
+        // IE Fails at this point, suspect I need to change context back to allow a re-click
         driver.findElement(By.cssSelector("a[target='compdev']")).click();
 
         driver.switchTo().window("compdev");
@@ -98,8 +109,7 @@ public class WindowsExercisesTest {
         assertTrue("Expected EvilTester.com", driver.getTitle().contains("EvilTester.com"));
 
         driver.switchTo().window(framesWindowHandle);
-        assertTrue("Expected Frames site",
-                driver.getTitle().contains("Frameset Example"));
+        assertTrue("Expected Frames site", driver.getTitle().contains("Frameset Example"));
 
         driver.switchTo().window("compdev");
         driver.close();
@@ -108,5 +118,10 @@ public class WindowsExercisesTest {
         driver.switchTo().window("evil");
         driver.close();
         assertEquals("Expected 1 window left", 1, driver.getWindowHandles().size());
+    }
+
+    @AfterClass
+    public static void quitToClean() {
+        driver.quit();
     }
 }
