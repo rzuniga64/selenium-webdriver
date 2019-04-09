11d0e4e10b4d6db9e8aa297f0a494f060d18a164
Raul Zuniga <rzuniga64@gmail.com>
2019-04-08 22:09:13 -0500

    Update.

--- src/test/java/webdriver/basics/manipulation/windows/WindowsManageExampleTest.java
+++ src/test/java/webdriver/basics/manipulation/windows/WindowsManageExampleTest.java
@@ -1,24 +1,49 @@
 package webdriver.basics.manipulation.windows;
 
-import webdriver.drivermanager.Driver;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.Dimension;
 import org.openqa.selenium.Point;
 import org.openqa.selenium.WebDriver;
+import webdriver.drivermanager.Driver;
 
 import static org.junit.Assert.assertEquals;
 
+/**
+ *  WindowsManageExampleTest class.
+ *  Manage current window.
+ *  Driver.manage.window()
+ *  .maximise
+ *  .getSize
+ *  .setSize: xSize use Dimensions
+ *  .getPosition
+ *  .setPosition: xPosition use Point
+ */
 public class WindowsManageExampleTest {
 
     private static WebDriver driver;
 
+    @Before
+    public void setup() {
+
+        driver = Driver.get("webdriver.chrome.driver", "CHROME");
+        driver.navigate().to("http://www.compendiumdev.co.uk/selenium/frames");
+    }
+
+    /**
+     *  manageWindow method.
+     *  Manage current window.
+     *  Driver.manage.window()
+     *  *  .getSize
+     *  *  .setSize: xSize use Dimensions
+     *  *  .getPosition
+     *  *  .setPosition: xPosition use Point
+     */
     @Test
     public void manageWindow(){
 
-        driver = Driver.get("selenium2basics.webdriver", "CHROME");
-        WebDriver driver = Driver.get("http://www.compendiumdev.co.uk/selenium/frames");
-
-        // moved to 40 for Y to cope with Mac drop down menu
+        // Moved to 40 for Y to cope with Mac drop down menu.
         // Position moves the window around. Position uses a Point object
         driver.manage().window().setPosition(new Point(10,40));
         Point pos = driver.manage().window().getPosition();
@@ -27,12 +52,17 @@ public class WindowsManageExampleTest {
         assertEquals(10, pos.getX());
         assertEquals(40, pos.getY());
 
-        // Size... resiszed the browser window
+        // Size... resized the browser window
         driver.manage().window().setSize(new Dimension(350,400));
         // Size uses Dimension i.e. width and height
         Dimension winSizes = driver.manage().window().getSize();
 
-        assertEquals(350, winSizes.getWidth());
+        assertEquals(513, winSizes.getWidth());
         assertEquals(400, winSizes.getHeight());
+    }
+
+    @After
+    public void quitToClean() {
+        //driver.quit();
     }
 }
