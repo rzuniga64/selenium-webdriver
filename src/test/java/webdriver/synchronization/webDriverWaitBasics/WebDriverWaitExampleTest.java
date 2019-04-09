0ae85b03252c7ac61c4f5bab6f2596f1c6e80761
Raul Zuniga <rzuniga64@gmail.com>
2019-04-09 10:25:26 -0500

    Update.

--- src/test/java/webdriver/synchronization/webDriverWaitBasics/WebDriverWaitExampleTest.java
+++ src/test/java/webdriver/synchronization/webDriverWaitBasics/WebDriverWaitExampleTest.java
@@ -1,41 +1,47 @@
 package webdriver.synchronization.webDriverWaitBasics;
 
 import org.junit.BeforeClass;
-import webdriver.drivermanager.Driver;
 import org.junit.Test;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import webdriver.drivermanager.Driver;
 
 import static org.junit.Assert.assertEquals;
+import static org.openqa.selenium.support.ui.ExpectedConditions.titleIs;
 
+/**
+ *  WebDriverWaitExampleTest class.
+ *  Write a test which navigates to compendiumdev.co.uk/selenium/basic_ajax.html and checks the title.
+ */
 public class WebDriverWaitExampleTest {
 
     private static WebDriver driver;
+    private static WebDriverWait wait;
 
     @BeforeClass
-    public static void createDriverAndVisitTestPage(){
-        driver = Driver.get("selenium2basics.webdriver", "CHROME");
+    public static void setup(){
+
+        driver = Driver.get("webdriver.chrome.driver", "CHROME");
+        driver.navigate().to("http://www.compendiumdev.co.uk/selenium/basic_ajax.html");
+        wait =  new WebDriverWait(driver,10);
     }
 
+    /**
+     *  exampleUsingExpectedConditions method.
+     *  Navigates to compendiumdev.co.uk/selenium/basic_ajax.html and checks the title.
+     */
     @Test
     public void exampleUsingExpectedConditions(){
 
-
-        driver = Driver.get("http://compendiumdev.co.uk/selenium/basic_html_form.html");
-
-        new WebDriverWait(driver,10).until(ExpectedConditions.titleIs("HTML Form Elements"));
-        assertEquals("HTML Form Elements", driver.getTitle());
+        wait.until(titleIs("Basic Ajax"));
+        assertEquals("Basic Ajax", driver.getTitle());
     }
 
     @Test
     public void exampleWithSleepTime(){
 
-        driver = Driver.get("http://compendiumdev.co.uk/selenium/basic_html_form.html");
-
-        new WebDriverWait(driver,10,50).until(
-                ExpectedConditions.titleIs("HTML Form Elements"));
-
-        assertEquals("HTML Form Elements", driver.getTitle());
+        wait =  new WebDriverWait(driver,10,50);
+        wait.until(titleIs("Basic Ajax"));
+        assertEquals("Basic Ajax", driver.getTitle());
     }
 }
