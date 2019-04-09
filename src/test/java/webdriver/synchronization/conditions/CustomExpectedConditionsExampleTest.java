ac82aeba5635614cde7a4af8e16920fbbd99d270
Raul Zuniga <rzuniga64@gmail.com>
2019-04-09 17:21:44 -0500

    Update.

--- src/test/java/webdriver/synchronization/conditions/CustomExpectedConditionsExampleTest.java
+++ src/test/java/webdriver/synchronization/conditions/CustomExpectedConditionsExampleTest.java
@@ -15,26 +15,36 @@ import java.util.List;
 import static org.junit.Assert.assertEquals;
 import static org.openqa.selenium.support.ui.ExpectedConditions.elementToBeClickable;
 
+/**
+ *  CustomExpectedConditionsExampleTest class.
+ *  Use a CustomExpectedCondition when ExpectedConditions does not have what you need.
+ *  You want to make your tests read well for your usage scenario
+ *  You want to pass additional values to the apply method
+ *
+ *  ...create a Custom ExpectedCondition
+ *  One method is a separate named class
+ */
 public class CustomExpectedConditionsExampleTest {
 
     private static WebDriver driver;
+    private static WebDriverWait wait;
 
     @BeforeClass
     public static void createDriverAndVisitTestPage(){
+
         driver = Driver.get("selenium2basics.webdriver", "CHROME");
+        driver.navigate().to("http://compendiumdev.co.uk/selenium/basic_ajax.html");
+        wait = new WebDriverWait(driver,10);
     }
 
     @Test
     public void customSynchronisationWithExpectedCondition(){
 
-            driver = Driver.get("http://compendiumdev.co.uk/selenium/basic_ajax.html");
-
             // select Server
             WebElement categorySelect = driver.findElement(By.id("combo1"));
             categorySelect.findElement(By.cssSelector("option[value='3']")).click();
 
-            new WebDriverWait(driver,10).until(
-                    new SelectContainsText(By.id("combo2"),"Java")
+            wait.until(new SelectContainsText(By.id("combo2"),"Java")
             );
 
             // then select Java
@@ -47,43 +57,59 @@ public class CustomExpectedConditionsExampleTest {
 
             // don't have to synchronise with other browsers but do with GeckoDriver
             //WebElement languageWeUsed = driver.findElement(By.id("_valuelanguage_id"));
-            WebElement languageWeUsed = new WebDriverWait(driver,10).until(elementToBeClickable( By.id("_valuelanguage_id")));
+            WebElement languageWeUsed = wait.until(elementToBeClickable( By.id("_valuelanguage_id")));
             assertEquals("Expected Java code", "23",languageWeUsed.getText());
     }
 
-
+    /**
+     *  SelectContainsText class.
+     *  Make it private it because it is local to the test.
+     *  Implements ExpectedCondition. You can return either Boolean or WebElement. I chose Boolean for this.
+     */
     private class SelectContainsText implements ExpectedCondition<Boolean> {
+
         private String textToFind;
         private By findBy;
 
+        // Pass in whatever you need in the constructor.
         private SelectContainsText(final By comboFindBy, final String textToFind) {
+
             this.findBy = comboFindBy;
             this.textToFind = textToFind;
         }
 
+        /**
+         *  Override apply. This is called by WebDriverWait.
+         * @param webDriver a WebDriver.
+         * @return a boolean.
+         */
         @Override
         public Boolean apply(WebDriver webDriver) {
+
             WebElement combo = webDriver.findElement(this.findBy);
             List<WebElement> options = combo.findElements(By.tagName("option"));
 
+            // Implement your checking code using the passed in WebDriver.
             for(WebElement anOption : options){
                 try{
                     if(anOption.getText().equals(this.textToFind))
                         return true;
-                }catch(StaleElementReferenceException e){
-                    // too fast, need to refresh the list so trigger a poll
-                    // and wait for next time
+                } catch(StaleElementReferenceException e){
+                    // too fast, need to refresh the list so trigger a poll and wait for next time
                     return false;
                 }
             }
-
             return false;
         }
 
+        /**
+         *  Convert object to a string.
+         * @return a string.
+         */
         @Override
         public String toString() {
+
             return "select " + this.findBy + " to contain " + this.textToFind;
         }
     }
-
 }
