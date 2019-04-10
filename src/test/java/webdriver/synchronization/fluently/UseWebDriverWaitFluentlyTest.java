a9b7e678baa17c948b850d54904166376f619e34
Raul Zuniga <rzuniga64@gmail.com>
2019-04-09 23:05:19 -0500

    Update.

--- src/test/java/webdriver/synchronization/fluently/UseWebDriverWaitFluentlyTest.java
+++ src/test/java/webdriver/synchronization/fluently/UseWebDriverWaitFluentlyTest.java
@@ -1,5 +1,7 @@
 package webdriver.synchronization.fluently;
 
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
 import webdriver.drivermanager.Driver;
 import org.junit.Test;
 import org.openqa.selenium.TimeoutException;
@@ -12,23 +14,46 @@ import java.util.concurrent.TimeUnit;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 
+/**
+ *  UseWebDriverWaitFluentlyTest class.
+ *  WebDriverWait is built on top of a class called FluentWait. That gives us the ability to construct a WebDriverWait that is more readable.
+ *  Additional methods:
+ *  .pollingEvery
+ *  .ignoring
+ *  .withTimeout
+ *  .withMessage
+ *  .ignoreAll
+ */
 public class UseWebDriverWaitFluentlyTest {
 
+    private static WebDriver driver;
+
+    @BeforeClass
+    public static void setup() {
+
+        driver = Driver.get("webdriver.chrome.driver", "CHROME");
+        driver.navigate().to("http://compendiumdev.co.uk");
+    }
+
+    /**
+     *  wait5Seconds method.
+     *  Uses additional FluentWait methods.  The apply function of the ExpectedCondition throws a IllegalStateException
+     *  every 100 ms so it never returns a Boolean true. Therefore it never comes out of its loop except when it times
+     *  out after 5 seconds.
+     */
     @Test
     public void wait5Seconds(){
-
-        WebDriver driver = Driver.get("http://compendiumdev.co.uk");
 
         long currTime = System.currentTimeMillis();
 
         try{
-
             /* this will ignore the thrown exception in the apply */
             new WebDriverWait(driver, 1).
-                    pollingEvery(100, TimeUnit.MILLISECONDS).
-                    ignoring(IllegalStateException.class).
-                    withTimeout(5, TimeUnit.SECONDS).
-                    withMessage("See I told you a Timeout Happened").until(
+                    pollingEvery(100, TimeUnit.MILLISECONDS).       // override polling time
+                    ignoring(IllegalStateException.class).                   // ignore exceptions
+                    withTimeout(5, TimeUnit.SECONDS).               // override timeout
+                    withMessage("See I told you a Timeout Happened").until(  // include text in Timeout Message
+                        // anonymous class throws an illegal state exception
                         new ExpectedCondition<Boolean>() {
                             @Override
                             public Boolean apply(WebDriver webDriver) {
@@ -39,7 +64,7 @@ public class UseWebDriverWaitFluentlyTest {
 
             fail("A time out exception should have been thrown");
 
-        }catch(TimeoutException e){
+        } catch (TimeoutException e){
             assertTrue(e.getMessage().contains("See I told you a Timeout Happened"));
         }
 
@@ -47,7 +72,10 @@ public class UseWebDriverWaitFluentlyTest {
         System.out.println("Timeout calculated as " + (nowTime - currTime));
 
         assertTrue((nowTime - currTime) >= 5000);
-
     }
 
+    @AfterClass
+    public static void closeBrowser() {
+        //driver.quit();
+    }
 }
