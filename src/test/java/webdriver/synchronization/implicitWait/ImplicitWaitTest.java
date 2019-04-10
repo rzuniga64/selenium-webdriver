40d1504217533b02f5b21eebf31983e295268207
Raul Zuniga <rzuniga64@gmail.com>
2019-04-10 13:45:30 -0500

    Update.

--- src/test/java/webdriver/synchronization/implicitWait/ImplicitWaitTest.java
+++ src/test/java/webdriver/synchronization/implicitWait/ImplicitWaitTest.java
@@ -16,25 +16,51 @@ import java.util.concurrent.TimeUnit;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
+/**
+ *  ImplicitWaitTest class.
+ *
+ *  Implicit wait
+ *  - .findElement has an implicit wait
+ *  - Default is 0
+ *  - Can amend the global default
+ *    driver.manage.timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+ *
+ *  Implicit or Explicit?
+ *  - Implicity can make initial tests faster to write
+ *    - You don’t have to worry about synchronization when writing tests
+ *  - It can be harder to add synchronization later
+ *    - You have to identify a source of intermittency
+ *  - If you start with implicit then you can expose synchronization problems by gradually reducing the implicit wait time.
+ */
 public class ImplicitWaitTest {
 
     private static WebDriver driver;
+    private static WebDriverWait wait;
 
     @Before
     public void setup(){
-        driver = Driver.get("http://compendiumdev.co.uk/selenium/" +
-                "basic_html_form.html");
+
+        driver = Driver.get("webdriver.chrome.driver", "CHROME");
+        driver.navigate().to("http://compendiumdev.co.uk/selenium/basic_html_form.html");
+        wait = new WebDriverWait(driver,10);
     }
 
+    /**
+     *  implicitWaitSynchronisationExample method.
+     *
+     *  - Implicit wait can make initial tests faster to write.
+     *    - You don’t have to worry about synchronization when writing tests
+     *  - It can be harder to add synchronization later
+     *    - You have to identify a source of intermittency
+     *  - If you start with implicit then you can expose synchronization problems by gradually reducing the implicit
+     *    wait time.
+     */
     @Test
-    // Exercise: Feel The Pain
-    public void chooseToCodeInJavaOnTheServerFromCombosImplicitWaitSynchronisationExample(){
+    public void implicitWaitSynchronisationExample(){
 
-        WebDriver driver;
+        driver.navigate().to("http://compendiumdev.co.uk/selenium/basic_ajax.html");
 
-        driver = Driver.get("http://compendiumdev.co.uk/selenium/" +
-                "basic_ajax.html");
-
+        // This is really not a good way to synchronize.
         driver.manage().timeouts().implicitlyWait(10,TimeUnit.SECONDS);
 
         // select Server
@@ -46,26 +72,28 @@ public class ImplicitWaitTest {
         languageSelect.findElement(By.cssSelector("option[value='23']")).click();
 
         // Submit the form
-        WebElement codeInIt = driver.findElement(By.name("submitbutton"));
-        codeInIt.click();
+        driver.findElement(By.name("submitbutton")).click();
 
         WebElement languageWeUsed = driver.findElement(By.id("_valuelanguage_id"));
         assertEquals("Expected Java code", "23",languageWeUsed.getText());
-
     }
 
+    /**
+     *  implicitWaitOfZeroTheMissingElementsAreFastToFind method.
+     *  Use implicit wait of 0 s. This is not a good synchronization strategy.
+     */
     @Test
-    public void withImplicitWaitOfZeroTheMissingElementsAreFastToFind(){
+    public void implicitWaitOfZeroTheMissingElementsAreFastToFind(){
 
         fillFormAndSubmit();
 
-        new WebDriverWait(driver,10).until(ExpectedConditions.titleIs("Processed Form Details"));
+        wait.until(ExpectedConditions.titleIs("Processed Form Details"));
 
         // wait for 0 seconds implicitly
         driver.manage().timeouts().implicitlyWait(0, TimeUnit.SECONDS);
 
         WebElement checkbox1Result;
-        WebElement checkbox3Result=null;
+        WebElement checkbox3Result = null;
 
         long findTimeStart = 0L;
         long findTimeEnd = 0L;
@@ -75,8 +103,7 @@ public class ImplicitWaitTest {
         findTimeEnd = System.currentTimeMillis();
 
         long totalTimeToFindElement = findTimeEnd - findTimeStart;
-
-        System.out.println("Time to find an element " + totalTimeToFindElement);
+        System.out.println("Time to find an element " + totalTimeToFindElement + " ms");
 
         long nofindTimeStart=0L;
         long nofindTimeend=0L;
@@ -85,7 +112,7 @@ public class ImplicitWaitTest {
             nofindTimeStart = System.currentTimeMillis();
             checkbox3Result = driver.findElement(By.id("_valuecheckboxes2"));
 
-        }catch(NoSuchElementException e){
+        } catch (NoSuchElementException e){
             // expected missing element
             nofindTimeend = System.currentTimeMillis();
         }
@@ -93,17 +120,18 @@ public class ImplicitWaitTest {
         long totalTimeToNotFindElement = nofindTimeend - nofindTimeStart;
 
         assertTrue("expected element not found", checkbox3Result==null);
-
-        System.out.println("Time to not find an element " + totalTimeToNotFindElement);
+        System.out.println("Time to not find an element " + totalTimeToNotFindElement + " ms");
 
         // time to find is variable so this check is not always true
-        //assertTrue("expected faster time to not find element",
-        //        totalTimeToNotFindElement < totalTimeToFindElement);
+        // assertTrue("expected faster time to not find element", totalTimeToNotFindElement < totalTimeToFindElement);
     }
 
-
+    /**
+     *  implicitWaitOfTenTheMissingElementsTakeAtLeastThatToNotFind methd.
+     *  Use implicit wait of 10 s. It will take at least that long to not find an element making tests longer.
+     */
     @Test
-    public void withImplicitWaitOfTenTheMissingElementsTakeAtLeastThatToNotFind(){
+    public void implicitWaitOfTenTheMissingElementsTakeAtLeastThatToNotFind(){
 
         fillFormAndSubmit();
 
@@ -124,7 +152,7 @@ public class ImplicitWaitTest {
 
         long totalTimeToFindElement = findTimeEnd - findTimeStart;
 
-        System.out.println("Time to find an element " + totalTimeToFindElement);
+        System.out.println("Time to find an element " + totalTimeToFindElement + " ms");
 
         long nofindTimeStart=0L;
         long nofindTimeend=0L;
@@ -140,7 +168,7 @@ public class ImplicitWaitTest {
 
         long totalTimeToNotFindElement = nofindTimeend - nofindTimeStart;
 
-        System.out.println("Time to not find an element " + totalTimeToNotFindElement);
+        System.out.println("Time to not find an element " + totalTimeToNotFindElement + " ms");
 
         // wait for 0 seconds implicitly by default
         driver.manage().timeouts().implicitlyWait(0, TimeUnit.SECONDS);
@@ -154,32 +182,20 @@ public class ImplicitWaitTest {
 
 
     private void fillFormAndSubmit() {
-        WebElement checkBox1;
-        WebElement checkBox3;
 
-        checkBox1 = driver.findElement(By.cssSelector("input[value='cb1']"));
-        checkBox3 = driver.findElement(By.cssSelector("input[value='cb3']"));
+        WebElement checkBox1 = driver.findElement(By.cssSelector("input[value='cb1']"));
+        WebElement checkBox3 = driver.findElement(By.cssSelector("input[value='cb3']"));
 
-        if(!checkBox1.isSelected()){
-            checkBox1.click();
-        }
+        if(!checkBox1.isSelected())checkBox1.click();
+        if(checkBox3.isSelected()) checkBox3.click();
 
-        if(checkBox3.isSelected()){
-            checkBox3.click();
-        }
-
-
-        WebElement submitButton;
-        submitButton = driver.findElement(
-                By.cssSelector(
-                        "input[type='submit'][name='submitbutton']"));
-
+        WebElement submitButton = driver.findElement(By.cssSelector("input[type='submit'][name='submitbutton']"));
         submitButton.click();
     }
 
     @After
     public void resetImplicitWait(){
+
         driver.manage().timeouts().implicitlyWait(0,TimeUnit.SECONDS);
     }
-
 }
