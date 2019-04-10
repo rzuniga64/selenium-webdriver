dff375349af71f2d3b210b16e9f34240fae0e33f
Raul Zuniga <rzuniga64@gmail.com>
2019-04-10 14:07:34 -0500

    Update.

--- src/test/java/webdriver/synchronization/refactored/MakeYourWaitsReadableTest.java
+++ src/test/java/webdriver/synchronization/refactored/MakeYourWaitsReadableTest.java
@@ -1,5 +1,6 @@
 package webdriver.synchronization.refactored;
 
+import org.junit.AfterClass;
 import webdriver.drivermanager.Driver;
 import org.junit.Before;
 import org.junit.Test;
@@ -12,22 +13,39 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 import static org.junit.Assert.assertEquals;
 import static org.openqa.selenium.support.ui.ExpectedConditions.elementToBeClickable;
 
+/**
+ *  MakeYourWaitsReadableTest class.
+ *
+ *  - Wrap an inline wait in a method
+ *    - e.g. ExpectedConditions
+ *  - Wrap an ExpectedCondition in a method
+ *  - Override toString
+ *  - Create a ‘wait’ variable
+ *    - Assign new WebDriverWait(...) in a @Before
+ *  - Import Expected Conditions statically
+ *    - Refactor to static import
+ *  - Wait for a ‘thing’ then use it
+ */
 public class MakeYourWaitsReadableTest {
 
-    WebDriver driver;
-    WebDriverWait wait;
+    private static WebDriver driver;
+    private static WebDriverWait wait;
 
     @Before
     public void setupForTest(){
 
-        driver = Driver.get("http://compendiumdev.co.uk/selenium/" +
-                "basic_ajax.html");
-
+        driver = Driver.get("webdriver.chrome.driver", "CHROME");
+        driver.navigate().to("http://compendiumdev.co.uk/selenium/basic_ajax.html");
         // instantiate your waits at the highest level you can, to reuse in the test
         wait = new WebDriverWait(driver,10);
-
     }
 
+    /**
+     *  wrapAnonymousClassesInMethods method.
+     *  - Wrap an inline wait in a method
+     *    - e.g. ExpectedConditions
+     *  - Wait for a ‘thing’ then use it
+     */
     @Test
     public void wrapAnonymousClassesInMethods(){
 
@@ -35,28 +53,31 @@ public class MakeYourWaitsReadableTest {
         categorySelect.findElement(By.cssSelector("option[value='3']")).click();
 
         // wait for a thing, then use it
-        wait.until(optionWithValueDisplayed("23")).click();
+        WebElement javaOption = wait.until(optionWithValueDisplayed("23"));
+        javaOption.click();
 
-
-        WebElement codeInIt = driver.findElement(By.name("submitbutton"));
-        codeInIt.click();
+        WebElement submitButton = driver.findElement(By.name("submitbutton"));
+        submitButton.click();
 
         // don't have to synchronise with other browsers but do with GeckoDriver
         //WebElement languageWeUsed = driver.findElement(By.id("_valuelanguage_id"));
         WebElement languageWeUsed = wait.until(elementToBeClickable( By.id("_valuelanguage_id")));
-
         assertEquals("Expected Java code", "23",languageWeUsed.getText());
-
     }
 
-    // wrap your anonymous classes in a method
-    // override toString
+    /**
+     *  - Wrap an inline wait in a method
+     *    - e.g. ExpectedConditions
+     *  - Override toString
+     * @param value the String to be searched for
+     * @return a WebElement
+     */
     private ExpectedCondition<WebElement> optionWithValueDisplayed(final String value) {
+
         return new ExpectedCondition<WebElement>() {
             @Override
             public WebElement apply(WebDriver webDriver) {
-                return webDriver.findElement(
-                        By.cssSelector("option[value='" + value + "']"));
+                return webDriver.findElement(By.cssSelector("option[value='" + value + "']"));
             }
 
             @Override
@@ -67,6 +88,11 @@ public class MakeYourWaitsReadableTest {
     }
 
 
+    /**
+     *  importExpectedConditionMethodsStatically
+     *  - Import Expected Conditions statically
+     *    - Refactor to static import
+     */
     @Test
     public void importExpectedConditionMethodsStatically(){
 
@@ -75,18 +101,20 @@ public class MakeYourWaitsReadableTest {
 
         // wait for a thing, then use it
         // static import for the elementToBeClickable method
-        WebElement javaOption = wait.until(elementToBeClickable(
-                                            By.cssSelector("option[value='23']")));
+        WebElement javaOption = wait.until(elementToBeClickable(By.cssSelector("option[value='23']")));
         javaOption.click();
 
         WebElement codeInIt = driver.findElement(By.name("submitbutton"));
         codeInIt.click();
 
         // don't have to synchronise with other browsers but do with GeckoDriver
-        //WebElement languageWeUsed = driver.findElement(By.id("_valuelanguage_id"));
+        // WebElement languageWeUsed = driver.findElement(By.id("_valuelanguage_id"));
         WebElement languageWeUsed = wait.until(elementToBeClickable( By.id("_valuelanguage_id")));
-
         assertEquals("Expected Java code", "23",languageWeUsed.getText());
     }
 
+    @AfterClass
+    public static void closeBrowser() {
+        //driver.quit();
+    }
 }
