d43c986f23191611075f69ed4bb1317477274657
Raul Zuniga <rzuniga64@gmail.com>
2019-04-11 14:31:39 -0500

    Update.

--- src/test/java/webdriver/drivers/FirefoxDriverTest.java
+++ src/test/java/webdriver/drivers/FirefoxDriverTest.java
@@ -1,6 +1,9 @@
 package webdriver.drivers;
 
 
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
 import webdriver.drivermanager.Driver;
 import manager.ProxyPort;
 import org.junit.Test;
@@ -17,52 +20,56 @@ import java.io.IOException;
 import static org.hamcrest.core.Is.is;
 import static org.hamcrest.MatcherAssert.assertThat;
 
+/**
+ *  FirefoxDriverTest class.
+ *  If we don't tell webdriver where to find the chrome driver it will look on the path.
+ */
 public class FirefoxDriverTest {
 
-    /*
-        Firefox version 48 has changed and requires GeckoDriver/Marionette.
-        This code will only work for versions of firefox 47.1 and below
-        You can download version 45 of firefox from the Extended Support Release page
-        https://www.mozilla.org/en-US/firefox/organizations/all/
+    private static WebDriver firefox;
+
+    @BeforeClass
+    public static void setupTheChromeDriverSystemProperty(){
+
+        firefox = new FirefoxDriver();
+    }
+
+    /**
+     *  basicFirefoxDriver method.
      */
     @Test
     public void basicFirefoxDriver(){
 
-        WebDriver firefox = new FirefoxDriver();
-
-        firefox.get("http://www.compendiumdev.co.uk/selenium/basic_html_form.html");
-
+        firefox.navigate().to("http://www.compendiumdev.co.uk/selenium/basic_html_form.html");
         assertThat(firefox.getTitle(), is("HTML Form Elements"));
-
-        firefox.quit();
     }
 
+    /**
+     *  firefoxDriverWithProfile method.
+     */
     @Test
     public void firefoxDriverWithProfile(){
 
         FirefoxProfile profile = new FirefoxProfile();
 
-        // setEnableNativeEvents no longer an option in 3.12.0
-        //profile.setEnableNativeEvents(true);
-        // WebDriver firefox = new FirefoxDriver(profile);
-
         // now need to use options to set the profile (3.12.0)
         FirefoxOptions options = new FirefoxOptions().setProfile(profile);
-        WebDriver firefox = new FirefoxDriver(options);
+        firefox = new FirefoxDriver(options);
 
-        firefox.get("http://www.compendiumdev.co.uk/selenium/basic_html_form.html");
-
+        firefox.navigate().to("http://www.compendiumdev.co.uk/selenium/basic_html_form.html");
         assertThat(firefox.getTitle(), is("HTML Form Elements"));
-
-        firefox.quit();
     }
 
-
+    /**
+     *  firefoxDriverWithCapabilitiesForProxy method.
+     *  Slightly newer.
+     */
     @Test
     public void firefoxDriverWithCapabilitiesForProxy(){
 
         //run this only if proxy is running e.g. Fiddler or BrowserMobProxy or BurpSuite etc.
         if(ProxyPort.inUse(Driver.PROXYHOST, Driver.PROXYPORT)) {
+
             org.openqa.selenium.Proxy proxy = new org.openqa.selenium.Proxy();
             proxy.setHttpProxy(Driver.PROXY)
                  .setFtpProxy(Driver.PROXY)
@@ -71,14 +78,10 @@ public class FirefoxDriverTest {
             DesiredCapabilities capabilities = new DesiredCapabilities();
             capabilities.setCapability(CapabilityType.PROXY, proxy);
 
-            WebDriver firefox = new FirefoxDriver(capabilities);
-
-            firefox.get("http://www.compendiumdev.co.uk/selenium/basic_html_form.html");
-
+            firefox = new FirefoxDriver(capabilities);
+            firefox.navigate().to("http://www.compendiumdev.co.uk/selenium/basic_html_form.html");
             assertThat(firefox.getTitle(), is("HTML Form Elements"));
-
-            firefox.quit();
-        }else{
+        } else {
             System.out.println(
                     "No Proxy seemed to be running on " +
                     Driver.PROXY +
@@ -86,6 +89,10 @@ public class FirefoxDriverTest {
         }
     }
 
+    /**
+     *  firefoxUseExtensions method.
+     *  @throws IOException throws IOException
+     */
     @Test
     public void firefoxUseExtensions() throws IOException {
 
@@ -99,11 +106,9 @@ public class FirefoxDriverTest {
                                 String.format("%ssrc%stest%sresources%s%s",s,s,s,s,"firebug-1.10.5-fx.xpi");
 
         System.out.println(extensionPath);
-
         FirefoxProfile profile = new FirefoxProfile();
 
-        // stop firebug showing the first run screen by setting the last version
-        // to the current one
+        // Stop firebug showing the first run screen by setting the last version to the current one.
         profile.setPreference("extensions.firebug.currentVersion", "1.10.5");
 
         // add the extension to firefox
@@ -117,11 +122,16 @@ public class FirefoxDriverTest {
         WebDriver firefox = new FirefoxDriver(options);
 
         firefox.get("http://www.compendiumdev.co.uk/selenium/basic_html_form.html");
-
         assertThat(firefox.getTitle(), is("HTML Form Elements"));
-
-        firefox.quit();
     }
 
+    @After
+    public void closeBrowser() {
+        firefox.close();
+    }
 
+    @AfterClass
+    public static void quitBrowser() {
+        //firefox.quit();
+    }
 }
