64d7211516161df38b7c6fb66460e4cff544fb5b
Raul Zuniga <rzuniga64@gmail.com>
2019-04-10 22:22:22 -0500

    Update.

--- src/test/java/webdriver/javascript/JavascriptExecuteAsyncExerciseTest.java
+++ src/test/java/webdriver/javascript/JavascriptExecuteAsyncExerciseTest.java
@@ -1,28 +1,36 @@
 package webdriver.javascript;
 
 
-import com.seleniumsimplified.webdriver.manager.Driver;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
+import webdriver.drivermanager.Driver;
 
 import java.util.concurrent.TimeUnit;
 
+import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.containsString;
 import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.assertThat;
 
-
+/**
+ *  JavascriptExecuteAsyncExerciseTest class.
+ *
+ *  Look at the JavaDoc help for executeAsyncScript
+ *  - Create a test for the sleep sample code
+ *  - Use the XMLHttpRequest sample to call /selenium/ajaxselect.php which is the server method which responds to the
+ *    Ajax call on the app. You will need to add an id parameter to call e.g. id=2.
+ */
 public class JavascriptExecuteAsyncExerciseTest {
 
     private static WebDriver driver;
 
-
     @BeforeClass
     public static void setup(){
-        driver = Driver.get("http://www.compendiumdev.co.uk/selenium/basic_ajax.html");
+
+        driver = Driver.get("webdriver.chrome.driver", "CHROME");
+        driver.navigate().to("http://www.compendiumdev.co.uk/selenium/basic_ajax.html");
     }
 
     @Before
@@ -31,26 +39,33 @@ public class JavascriptExecuteAsyncExerciseTest {
         driver.navigate().refresh();
     }
 
+    /**
+     *  waitInBrowserForTimeSample method.
+     *  Look at the JavaDoc help for executeAsyncScript
+     *  - Create a test for the sleep sample code
+     */
     @Test
     public void waitInBrowserForTimeSample(){
 
         driver.manage().timeouts().setScriptTimeout(10, TimeUnit.SECONDS);
 
         long start = System.currentTimeMillis();
-        ((JavascriptExecutor) driver).executeAsyncScript(
-                "window.setTimeout(arguments[arguments.length - 1], 500);");
+        ((JavascriptExecutor) driver).executeAsyncScript("window.setTimeout(arguments[arguments.length - 1], 500);");
 
-        System.out.println(
-                "Elapsed time: " + (System.currentTimeMillis() - start));
-
+        System.out.println("Elapsed time: " + (System.currentTimeMillis() - start));
         assertTrue("Elapsed time should be greater than 500 milli",
                 (System.currentTimeMillis() - start) > 500);
-
     }
 
+    /**
+     *  useXMLHttpRequest method.
+     *
+     *  Look at the JavaDoc help for executeAsyncScript
+     *  - Use the XMLHttpRequest sample to call /selenium/ajaxselect.php which is the server method which responds to the
+     *    Ajax call on the app. You will need to add an id parameter to call e.g. id=2.
+     */
     @Test
     public void useXMLHttpRequest(){
-
 
         driver.manage().timeouts().setScriptTimeout(10, TimeUnit.SECONDS);
 
@@ -66,14 +81,8 @@ public class JavascriptExecuteAsyncExerciseTest {
                         "xhr.send();");
 
         System.out.println((String)response);
-
-        assertThat((String) response,
-                containsString("{optionValue:10, optionDisplay: 'C++'}"));
-
+        assertThat((String) response, containsString("{optionValue:10, optionDisplay: 'C++'}"));
     }
-
-
-
 }
 
 
