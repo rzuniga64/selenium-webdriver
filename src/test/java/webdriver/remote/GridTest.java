282d56a3a2a0edb159c3d0ae070fbf3883eeba87
Raul Zuniga <rzuniga64@gmail.com>
2019-04-12 16:33:49 -0500

    Update.

--- src/test/java/webdriver/remote/GridTest.java
+++ src/test/java/webdriver/remote/GridTest.java
@@ -16,6 +16,21 @@ import java.net.URL;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
+/**
+ *  GridTest class.
+ *  1. On the command line navigate to  C:\Users\Owner\git\selenium-webdriver\src\test\resources\gridconfig\win.
+ *  2. Open starthub.bat and startnode and set the correct version of selenium-standalone jar.
+ *  3. On the command line type: starthub. This will start the server we will be connecting to with our tests.
+ *  4. On another command line type: startnode. This will register a node with the hub. The node starts on a port and
+ *     starts polling.
+ *  5. In a browser type: http://localhost:4444/grid/console. The grid console is for the hub. You can see that that
+ *     there is a node that is available for me to test. I can test against IE, Firefox, and Chrome.
+ *
+ *  Create your test and run it. You can run GridTest as an example. It runs on Windows/Firefox by default.
+ *  If you want to run on Chrome we have to tell the node about the Chrome driver. You can set it up in your tests.
+ *  You can either set up the properties on the machine that the node is running on or we can pass it in through the
+ *  command line. So you have to configure the node and tell it where the chrome driver is.
+ */
 public class GridTest {
 
     public static WebDriver driver = null;
