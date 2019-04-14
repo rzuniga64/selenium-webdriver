4cb9a9665bc0b82281a4971a4126d854b52a9857
Raul Zuniga <rzuniga64@gmail.com>
2019-04-13 23:00:05 -0500

    Update.

---
+++ src/test/java/webdriver/drivermanager/DriverTest.java
@@ -0,0 +1,45 @@
+package webdriver.drivermanager;
+
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+
+public class DriverTest {
+
+    WebDriver driver;
+
+    @Test
+    public void createAnHtmlUnitDriver(){
+
+        driver = Driver.get(Driver.BROWSER_PROPERTY_NAME,"HTMLUNIT" );
+        assertBrowserTestRuns();
+    }
+
+    @Test
+    public void createAFirefoxDriver(){
+
+        System.setProperty(Driver.BROWSER_PROPERTY_NAME, "FIREFOX");
+        driver = Driver.get(Driver.BROWSER_PROPERTY_NAME,"FIREFOX" );
+        assertBrowserTestRuns();
+    }
+
+    @Test
+    public void createAChromeDriver(){
+        driver = Driver.get(Driver.BROWSER_PROPERTY_NAME,"CHROME" );
+        assertBrowserTestRuns();
+    }
+
+    @Test
+    public void createAEdgeDriver(){
+        driver = Driver.get(Driver.BROWSER_PROPERTY_NAME,"EDGE" );
+        assertBrowserTestRuns();
+    }
+
+    public void assertBrowserTestRuns(){
+
+        driver.get("http://compendiumdev.co.uk/selenium/basic_web_page.html");
+        assertThat(driver.getTitle(), is("Basic Web Page Title"));
+    }
+}
