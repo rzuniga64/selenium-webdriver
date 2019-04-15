b73da48d325c964141a405d14142425e66fb3ca2
Raul Zuniga <rzuniga64@gmail.com>
2019-04-14 21:25:23 -0500

    Update.

--- src/test/java/webdriver/pageobjects/refactorExampleOne/BasicTestsRefactored.java
+++ src/test/java/webdriver/pageobjects/refactorExampleOne/BasicTestsRefactored.java
@@ -1,15 +1,14 @@
 package webdriver.pageobjects.refactorExampleOne;
 
-import com.seleniumsimplified.webdriver.manager.Driver;
-import com.seleniumsimplified.webdriver.pageobjects.refactorExampleOne.pages.BasicAjaxPageObject;
-import com.seleniumsimplified.webdriver.pageobjects.refactorExampleOne.pages.ProcessedFormPage;
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.openqa.selenium.WebDriver;
+import webdriver.drivermanager.Driver;
+import webdriver.pageobjects.refactorExampleOne.pages.BasicAjaxPageObject;
+import webdriver.pageobjects.refactorExampleOne.pages.ProcessedFormPage;
 
 import static org.junit.Assert.assertEquals;
-import static com.seleniumsimplified.webdriver.pageobjects.refactorExampleOne.pages.BasicAjaxPageObject.Category;
+import static webdriver.pageobjects.refactorExampleOne.pages.BasicAjaxPageObject.Category;
 
 public class BasicTestsRefactored {
 
@@ -17,53 +16,58 @@ public class BasicTestsRefactored {
     private BasicAjaxPageObject basicAjaxPage;
 
     @Before
-    public void setupTest(){
-        driver = Driver.get();
+    public void setup(){
+
+        driver = Driver.get("webdriver.chrome.driver", "CHROME");
         basicAjaxPage = new BasicAjaxPageObject(driver);
         basicAjaxPage.get();
     }
 
+    /**
+     *  Select the Server category and Java language in the form and submit and assert language is correct.
+     */
     @Test
     public void chooseToCodeInJavaOnTheServerFromCombosSyncOnAjaxBusyExample(){
 
         basicAjaxPage.selectCategory(Category.SERVER);
         basicAjaxPage.selectLanguage(23);
-        basicAjaxPage.clickCodeInIt();
+        basicAjaxPage.clickSubmitButton();
 
         ProcessedFormPage processedFormPage = new ProcessedFormPage(driver);
         processedFormPage.waitUntilPageIsLoaded();
 
         assertEquals("Expected Java code", "23",processedFormPage.getValueFor("language_id"));
-
-
     }
 
-    @Ignore("exercise: investigate and fix this broken test")
+    /**
+     *  Select the Web category and JavaScript language in the form and submit and assert language is correct.
+     */
     @Test
     public void chooseToCodeInJavascriptOnTheWeb(){
 
         basicAjaxPage.selectCategory(Category.WEB);
         basicAjaxPage.selectLanguage(0);
-        basicAjaxPage.clickCodeInIt();
+        basicAjaxPage.clickSubmitButton();
 
         ProcessedFormPage processedFormPage = new ProcessedFormPage(driver);
         processedFormPage.waitUntilPageIsLoaded();
 
-        assertEquals("Expected Javascript code", "0",processedFormPage.getValueFor("language_id"));
-
+        assertEquals("Expected Javascript code", "0", processedFormPage.getValueFor("language_id"));
     }
 
+    /**
+     *  Select the Desktop category and C++ language in the form and submit and assert language is correct.
+     */
     @Test
     public void chooseToCodeInCppOnDesktop(){
 
         basicAjaxPage.selectCategory(Category.DESKTOP);
         basicAjaxPage.selectLanguage(10);
-        basicAjaxPage.clickCodeInIt();
+        basicAjaxPage.clickSubmitButton();
 
         ProcessedFormPage processedFormPage = new ProcessedFormPage(driver);
         processedFormPage.waitUntilPageIsLoaded();
 
         assertEquals("Expected Cpp code", "10",processedFormPage.getValueFor("language_id"));
     }
-
 }
