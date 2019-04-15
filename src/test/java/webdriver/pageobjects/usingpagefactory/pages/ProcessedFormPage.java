45dd963caa1e09b114950389307024891f336e62
Raul Zuniga <rzuniga64@gmail.com>
2019-04-14 23:18:06 -0500

    Update.

--- src/test/java/webdriver/pageobjects/usingpagefactory/pages/ProcessedFormPage.java
+++ src/test/java/webdriver/pageobjects/usingpagefactory/pages/ProcessedFormPage.java
@@ -3,21 +3,38 @@ package webdriver.pageobjects.usingpagefactory.pages;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.LoadableComponent;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+import static org.openqa.selenium.support.ui.ExpectedConditions.titleIs;
+
 public class ProcessedFormPage extends LoadableComponent<ProcessedFormPage>{
+
     private WebDriver driver;
+    private WebDriverWait wait;
 
+    /**
+     *  Constructor.
+     *  @param aDriver the WebDriver for the browser under test.
+     */
     public ProcessedFormPage(WebDriver aDriver) {
+
         driver = aDriver;
+        wait = new WebDriverWait(driver, 10);
     }
 
+    /**
+     *  Wait until process form page is loaded.
+     */
     public void waitUntilPageIsLoaded() {
-        new WebDriverWait(driver,10).until(ExpectedConditions.titleIs("Processed Form Details"));
+        wait.until(titleIs("Processed Form Details"));
     }
 
+    /**
+     *  Get the value of a WebElement give the id on the process form page.
+     *  @param valueID the id of the WebElement.
+     *  @return the value of the WebElement.
+     */
     public String getValueFor(String valueID) {
         WebElement fieldValueElement = driver.findElement(By.id("_value" + valueID));
         return fieldValueElement.getText();
@@ -29,8 +46,13 @@ public class ProcessedFormPage extends LoadableComponent<ProcessedFormPage>{
         // We will never load a processed form, it will always be a result of another action
     }
 
+    /**
+     *  Check if page is loaded.
+     *  @throws Error error.
+     */
     @Override
     protected void isLoaded() throws Error {
+
         if(!driver.getTitle().contentEquals("Processed Form Details")){
             throw new Error("Title was not 'Processed Form Details' it was " + driver.getTitle());
             //or
