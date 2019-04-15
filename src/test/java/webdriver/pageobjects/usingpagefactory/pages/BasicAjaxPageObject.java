176132ff3366646b7aa4193ecd9108283da2678f
Raul Zuniga <rzuniga64@gmail.com>
2019-04-14 23:13:29 -0500

    Update.

--- src/test/java/webdriver/pageobjects/usingpagefactory/pages/BasicAjaxPageObject.java
+++ src/test/java/webdriver/pageobjects/usingpagefactory/pages/BasicAjaxPageObject.java
@@ -7,7 +7,6 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.How;
 import org.openqa.selenium.support.PageFactory;
-import org.openqa.selenium.support.pagefactory.AjaxElementLocatorFactory;
 import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.LoadableComponent;
@@ -16,17 +15,19 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 public class BasicAjaxPageObject extends LoadableComponent<BasicAjaxPageObject> {
 
     WebDriver driver;
+    private WebDriverWait wait;
 
-    @FindBy(how= How.ID, using="combo1")
+    @FindBy(how = How.ID, using="combo1")
     private WebElement categorySelect;
 
-    @FindBy(how= How.ID, using="combo2")
+    @FindBy(how = How.ID, using="combo2")
     private WebElement languageSelect;
 
     @FindBy(how= How.NAME, using="submitbutton")
-    private WebElement codeInIt;
+    private WebElement submitButton;
 
     public enum Category{
+
         WEB(1), DESKTOP(2), SERVER(3);
 
         private int dropDownValue;
@@ -40,7 +41,8 @@ public class BasicAjaxPageObject extends LoadableComponent<BasicAjaxPageObject> 
         }
     }
 
-    public enum Language {
+    public static enum Language {
+
         JAVASCRIPT(0), VBSCRIPT(1), FLASH(2),
         COBOL(20), FORTRAN(21), SERVER_Cpp(22), JAVA(23),
         DESKTOP_Cpp(10), ASSEMBLER(11), C(12), VISUAL_BASIC(13);
@@ -56,42 +58,70 @@ public class BasicAjaxPageObject extends LoadableComponent<BasicAjaxPageObject> 
         }
     }
 
+    /**
+     *  Constructor
+     *  @param webDriver the WebDriver for the browser under test.
+     */
     public BasicAjaxPageObject(WebDriver webDriver) {
+
         driver = webDriver;
+        wait = new WebDriverWait(driver, 10);
         PageFactory.initElements(driver, this);
     }
 
+    /**
+     *  Navigate to the URL.
+     */
     @Override
     protected void load() {
-        driver.get("http://compendiumdev.co.uk/selenium/basic_ajax.html");
+
+        driver.navigate().to("http://compendiumdev.co.uk/selenium/basic_ajax.html");
     }
 
+    /**
+     *  Is category selected loaded.
+     * @throws Error error.
+     */
     @Override
     protected void isLoaded() throws Error {
 
         try{
             categorySelect.isDisplayed();
-        }catch(NoSuchElementException e){
+        } catch(NoSuchElementException e) {
             throw new Error("basic_ajax page not loaded");
         }
     }
 
+    /**
+     *  Select category from dropdown.
+     *  @param category the category.
+     */
     public void selectCategory(Category category) {
+
         categorySelect.findElement(By.cssSelector("option[value='" + category.value() + "']")).click();
-        new WebDriverWait(driver,10).until(ajaxActionIsComplete());
+        wait.until(ajaxActionIsComplete());
     }
 
+    /**
+     *  Wait until the ajax symbol has gone because then the drop down has populated.
+     *  @return an ExpectedCondition.
+     */
     public ExpectedCondition<Boolean> ajaxActionIsComplete() {
+
         return ExpectedConditions.invisibilityOfElementLocated(
                 By.id("ajaxBusy"));
     }
 
+    /**
+     *  Select the programming language.
+     *  @param language the programming language.
+     */
     public void selectLanguage(Language language) {
         languageSelect.findElement(By.cssSelector("option[value='" + language.value() + "']")).click();
     }
 
     public void clickCodeInIt() {
-        codeInIt.click();
+        submitButton.click();
     }
 
 }
