0b8f447918ead1b59fcf1c8a112d17d81b525174
Raul Zuniga <rzuniga64@gmail.com>
2019-04-19 01:41:33 -0500

    Update.

--- src/test/java/webdriver/synchronization/fluently/UseWebDriverWaitFluentlyTest.java
+++ src/test/java/webdriver/synchronization/fluently/UseWebDriverWaitFluentlyTest.java
@@ -32,7 +32,7 @@ public class UseWebDriverWaitFluentlyTest {
     @BeforeClass
     public static void setup() throws IOException {
 
-        driver = Driver.get("webdriver.chrome.driver", "CHROME");
+        driver = Driver.get();
         driver.navigate().to("http://compendiumdev.co.uk");
     }
 
