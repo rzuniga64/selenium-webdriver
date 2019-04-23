7ea695c569f8759ef6811c1bc8d48c41100d25c7
Raul Zuniga <rzuniga64@gmail.com>
2019-04-23 13:34:39 -0500

    Initial revision.

---
+++ src/test/java/webdriver/driver_manager/DriverType.java
@@ -0,0 +1,16 @@
+package webdriver.driver_manager;
+
+public enum DriverType {
+    HTMLUNIT,
+    FIREFOX,
+    CHROME,
+    GOOGLECHROME,
+    EDGE,
+    IE,
+    OPERA,
+    FIREFOXPORTABLE,
+    FIREFOXMARIONETTE,
+    SAUCELABS,
+    GRID,
+    APPIUM
+}
