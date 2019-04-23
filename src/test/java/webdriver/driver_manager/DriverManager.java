7a24fa99823579112c09fc5d1a4a15c816367ee3
Raul Zuniga <rzuniga64@gmail.com>
2019-04-23 14:15:03 -0500

    Update.

---
+++ src/test/java/webdriver/driver_manager/DriverManager.java
@@ -0,0 +1,70 @@
+package webdriver.driver_manager;
+
+import org.openqa.selenium.WebDriver;
+
+/**
+ *  A abstract driver manager to maintain Drivers. Since all browsers that most people will use now essentially need an
+ *  'external' driver. I am making Chrome the default.
+ *
+ *  The reason for this class is to limit the time it takes to start the browser. When the tests are run typically all
+ *  the tests classes are run. This class allows sharing of the driver between the tests so minimize start up and
+ *  shutdown of the browser.
+ *
+ *   - 19th October 2016
+ */
+public abstract class DriverManager extends Thread {
+
+    static final String BROWSER_PROPERTY_NAME = "selenium2basics.webdriver";
+    private static final String DEFAULT_BROWSER = "CHROME";
+    public static final long DEFAULT_TIMEOUT_SECONDS = 10;
+
+    public static DriverType currentDriver;
+    public static String RESOURCE_DIR = "";
+    public static String BROWSER = DEFAULT_BROWSER;
+    public static String PROXYHOST = "localhost";
+    public static String PROXYPORT = "8888";
+    public static String PROXY = PROXYHOST + ":" + PROXYPORT;
+
+    protected static long browserStartTime = 0L;
+    protected static long savedTimecount = 0L;
+    protected static WebDriver driver = null;
+    protected static DriverType useThisDriver = null;
+
+    protected abstract void startService();
+    protected abstract void stopService();
+    protected abstract void createDriver();
+
+    /**
+     *  getDriver method.
+     *  We can put this into continuous integration and call it from system property and we can control it within tests.
+     *  Grow the abstraction layers on the needs that we have.
+     *  Currently we don't have any capabilities. All the browsers are eventually going to start using capabilities as
+     *  our default control mechanism. We can't support that in here. We will have to find a way of doing that.
+     *
+     *  @return a WebDriver
+     */
+    public WebDriver getDriver() {
+
+        if (driver == null) {
+            startService();
+            createDriver();
+        }
+
+        return driver;
+    }
+
+    /**
+     *  If there is a driver, quit it.
+     */
+    public void quitDriver() {
+
+        if (driver != null) {
+            try {
+                driver.quit();
+                driver = null;
+            } catch(Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
