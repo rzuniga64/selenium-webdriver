9e17de12c0cd087dc134e9c126ce5b9709bb06f6
Raul Zuniga <rzuniga64@gmail.com>
2019-04-24 14:28:15 -0500

    Update.

---
+++ src/test/java/webdriver/driver_manager/SauceLabsDriver.java
@@ -0,0 +1,42 @@
+package webdriver.driver_manager;
+
+import org.openqa.selenium.Platform;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+/**
+ *  Factory class which returns a SauceLabs driver.
+ */
+public class SauceLabsDriver extends DriverManager {
+    @Override
+    protected WebDriver createDriver() {
+
+        WebDriver saucelabsDriver = null;
+
+        DesiredCapabilities capabilities = DesiredCapabilities.firefox();
+        capabilities.setCapability("version", "5");
+        capabilities.setCapability("platform", Platform.WIN10);
+        try {
+            // add URL to environment variables to avoid releasing with source
+            String saucelabURL = System.getenv("SAUCELABS_URL");
+            saucelabsDriver = new RemoteWebDriver(new URL(saucelabURL), capabilities);
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+        return saucelabsDriver;
+    }
+
+    @Override
+    protected void startService() {
+
+    }
+
+    @Override
+    protected void stopService() {
+
+    }
+}
